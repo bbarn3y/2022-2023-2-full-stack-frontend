4ad5a7ac9681c99507733bcaa2818901177137ab
bbarn3y <davallik@hotmail.com>
2023-04-24 15:12:53 +0200

    Gyak 1

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,18 @@
+import {AfterViewInit, Component, OnChanges, OnDestroy, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.less']
+})
+export class LoginComponent implements OnInit, AfterViewInit, OnDestroy {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+  }
+
+}
